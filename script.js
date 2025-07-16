0e5f4c37542ff47c032b79e37fb8b9fe45e3403a
atypikos <sergi.rvr@gmail.com>
2025-07-16 09:31:28 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+// Números:
+// 1. Escribe una función que tome dos números como argumentos y devuelva su suma.
+
+function sumar (a,b) {
+    return a + b;
+}
+
+let resultadoSuma = sumar(3,10);
+    console.log(resultadoSuma);
+    
+// 2. Escribe una función que tome dos números como argumentos y devuelva su resta.
+
+function restar (a,b) {
+    return a - b;
+}
+
+let resultadoResta = restar(3,10);
+    console.log(resultadoResta);
+
+// 3. Escribe una función que tome dos números como argumentos y devuelva su producto.
+// 4. Escribe una función que tome dos números como argumentos y devuelva su división.
+// 5. Escribe una función que tome un número base y un exponente y devuelva el resultado de elevar el número base a la potencia dada.
+// 6. Escribe una función que tome dos números como argumentos y devuelva el resto de la división del primer número entre el segundo.
+// 7. Escribe una función que tome un número como argumento y devuelva su raíz cuadrada.
+// 8. Escribe una función que tome un número como argumento y devuelva su valor absoluto.
+// 9. Escribe una función que tome un número decimal como argumento y devuelva el número redondeado al entero más cercano.
+// 10. Escribe una función que genere y devuelva un número aleatorio entre 0 y 1.
