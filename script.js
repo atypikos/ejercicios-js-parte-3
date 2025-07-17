a1c6af5082ff5392d35c21d2a103301d19848c1d
atypikos <sergi.rvr@gmail.com>
2025-07-17 09:01:40 +0200

    letras_enunciado

--- script.js
+++ script.js
@@ -87,3 +87,14 @@ function random () {
 
 let resultadoRandom = random();
     console.log(resultadoRandom);
+
+// 1. Escribe una función que tome dos cadenas de texto como argumentos y devuelva su concatenación.
+// 2. Escribe una función que tome una cadena de texto como argumento y devuelva su longitud.
+// 3. Escribe una función que tome una cadena de texto como argumento y devuelva la misma cadena en mayúsculas.
+// 4. Escribe una función que tome una cadena de texto como argumento y devuelva la misma cadena en minúsculas.
+// 5. Escribe una función que tome una cadena de texto y un índice como argumentos, y devuelva el carácter en esa posición.
+// 6. Escribe una función que tome una cadena de texto como argumento y devuelva la misma cadena invertida.
+// 7. Escribe una función que tome una cadena de texto y un carácter como argumentos, y devuelva la cantidad de veces que el carácter aparece en la cadena.
+// 8. Escribe una función que tome una cadena de texto como argumento y devuelva la misma cadena sin espacios en blanco.
+// 9. Escribe una función que tome una cadena de texto como argumento y devuelva true si es un palíndromo (se lee igual de izquierda a derecha y de derecha a izquierda), o false si no lo es.
+// 10. Escribe una función que tome una cadena de texto como argumento y devuelva la misma cadena con la primera letra de cada palabra en mayúscula.    
