f5e00e46c0c0b0f23d28fe669ad958e44073bc99
atypikos <sergi.rvr@gmail.com>
2025-07-21 08:52:45 +0200

    Finalizado_textos_10

--- script.js
+++ script.js
@@ -189,3 +189,22 @@ let testeoPal = "hola";
 console.log(palindromo(testeoPal));
 // 10. Escribe una función que tome una cadena de texto como argumento y devuelva la misma cadena con la primera letra de cada palabra en mayúscula.    
 
+function cadena(texto) {
+        return texto
+        .split(' ') //split(' ') divide el texto en un array de palabras.
+        .map(palabra => palabra.charAt(0).toUpperCase() + palabra.slice(1)) //map(...) recorre cada palabra.
+        .join(' '); //join(' ') vuelve a unir las palabras con espacios.
+}
+
+console.log(cadena("por ejemplo quiero estas en mayúsculas"));
+
+// 1. Escribe una función que tome un array de números como argumento y devuelva la suma de todos sus elementos.
+// 2. Escribe una función que tome un array de números como argumento y devuelva el promedio de todos sus elementos.
+// 3. Escribe una función que tome un array de números como argumento y devuelva el array ordenado de forma ascendente.
+// 4. Escribe una función que tome un array de números y un número como argumentos, y devuelva un nuevo array con los elementos mayores al número dado.
+// 5. Escribe una función que tome dos arrays como argumentos y devuelva un nuevo array con todos los elementos de ambos arrays.
+// 6. Escribe una función que tome un array de números como argumento y devuelva el número máximo dentro del array.
+// 7. Escribe una función que tome un array de números como argumento y devuelva el número mínimo dentro del array.
+// 8. Escribe una función que tome un array y un elemento como argumentos, y devuelva la cantidad de veces que el elemento aparece en el array.
+// 9. Escribe una función que tome un array como argumento y devuelva un nuevo array sin elementos duplicados.
+// 10. Escribe una función que tome un array como argumento y devuelva un nuevo array con los elementos en orden inverso.
