9631884f8d9e6e6aa5d0432dbabc5086fc989758
atypikos <sergi.rvr@gmail.com>
2025-07-21 13:47:26 +0200

    literales_1

--- script.js
+++ script.js
@@ -308,3 +308,25 @@ function array10(reverse) {
 }
 
 console.log(array10([1,3,5,4,10]));
+
+
+// 1. Escribe una función que tome un objeto literal con una propiedad "nombre" y devuelva el valor de esa propiedad.
+
+let namerino = {
+    nombre: "Sergi"
+};
+
+
+console.log(namerino.nombre);
+
+// 2. Escribe una función que tome un objeto literal con una propiedad "edad" y un número como argumentos, y actualice el valor de la propiedad "edad" con el número dado.
+
+
+// 3. Escribe una función que tome un objeto literal y una cadena de texto como argumentos, y agregue una nueva propiedad al objeto con la cadena de texto como nombre y un valor inicial de null.
+// 4. Escribe una función que tome un objeto literal y una cadena de texto como argumentos, y elimine la propiedad del objeto con el nombre dado.
+// 5. Escribe una función que tome un objeto literal como argumento y devuelva la cantidad de propiedades que tiene.
+// 6. Escribe una función que tome un objeto literal y una cadena de texto como argumentos, y devuelva true si el objeto tiene una propiedad con ese nombre, o false si no la tiene.
+// 7. Escribe una función que tome un objeto literal como argumento y devuelva un array con todos los valores de sus propiedades.
+// 8. Escribe una función que tome dos objetos literales como argumentos y devuelva true si tienen las mismas propiedades y los mismos valores en esas propiedades, o false si son diferentes.
+// 9. Escribe una función que tome un objeto literal como argumento y devuelva una copia exacta de ese objeto.
+// 10. Escribe una función que tome dos objetos literales como argumentos y devuelva un nuevo objeto con todas las propiedades de ambos objetos. Si hay propiedades con el mismo nombre, el valor del segundo objeto deberá prevalecer.
